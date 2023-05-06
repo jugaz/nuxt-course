b8602e9f5d65438c7978734fa15a7e1df4879c95
Jesús Ugaz Vegas <jesusugazv@gmail.com>
2023-05-06 14:46:39 -0500

    config nuxt + vite

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+const path = require('path');
+export default defineNuxtConfig({
+    app: {
+        baseURL: "/nuxt-course/",
+    },
+    nitro: {
+        output: {
+            publicDir: path.join(__dirname, 'docs/')
+        }
+    },
+})
