2722a145b0ef738ba18e3077f1ca7ee7ce58dcba
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2023-05-18 15:33:58 -0500

    compile generate

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,10 +1,13 @@
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
     <div class="2xl:max-w-screen-2xl xl:max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="#" class="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ props.title }}</span>
-      </a>
+      <nuxt-link to="/"
+          activeClass="md:text-blue-700"
+          class="flex block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ props.title }}</span>
+      </nuxt-link>
+     
       <button
         data-collapse-toggle="navbar-dropdown"
         type="button"
